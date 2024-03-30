179897fdeddb7f17dff00d405cdb4ba2c29b6bb6
mopeneko <mopeneko@lem0n.cc>
2024-03-31 08:03:13 +0900

    Apply Biome changes

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import health from "./health"
+import health from "./health";
 
 const app = new Hono();
 
--- app/rollup.config.mjs
+++ app/rollup.config.mjs
@@ -1,15 +1,12 @@
-import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
 
 export default {
-    input: "api/index.ts",
-    output: {
-        file: "build/api.js",
-        format: "esm",
-        sourcemap: true
-    },
-    plugins: [
-        typescript(),
-        commonjs(),
-    ]
-}
+	input: "api/index.ts",
+	output: {
+		file: "build/api.js",
+		format: "esm",
+		sourcemap: true,
+	},
+	plugins: [typescript(), commonjs()],
+};
