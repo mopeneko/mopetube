ab3ce6cfd18d5d2a5df6348cf3c9a5eeb841b25c
mopeneko <mopeneko@lem0n.cc>
2024-04-01 20:04:33 +0900

    Create ECS stacks with using Terraform

---
+++ tf/variables.tf
@@ -0,0 +1,32 @@
+variable "github_owner" {
+  type = string
+  description = "GitHub repository owner"
+  default = "mopeneko"
+}
+
+variable "package_name" {
+  type = string
+  description = "Package name"
+  default = "app"
+}
+
+variable "domain_name" {
+    type = string
+    description = "Domain name"
+    default = "mopetube.com"
+}
+
+variable "vpc_flow_log_role_arn" {
+    type = string
+    description = "VPC Flow Log role ARN"
+}
+
+variable "ecs_task_execution_role_arn" {
+    type = string
+    description = "ECS task execution role ARN"
+}
+
+variable "github_token" {
+    type = string
+    description = "GitHub token"
+}
