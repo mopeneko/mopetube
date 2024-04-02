cee0e74a21f6d864779155d4ce302b3b7e792e01
mopeneko <mopeneko@lem0n.cc>
2024-04-03 00:20:04 +0900

    tf fmt

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,32 +1,32 @@
 variable "github_owner" {
-  type = string
+  type        = string
   description = "GitHub repository owner"
-  default = "mopeneko"
+  default     = "mopeneko"
 }
 
 variable "package_name" {
-  type = string
+  type        = string
   description = "Package name"
-  default = "app"
+  default     = "app"
 }
 
 variable "domain_name" {
-    type = string
-    description = "Domain name"
-    default = "mopetube.com"
+  type        = string
+  description = "Domain name"
+  default     = "mopetube.com"
 }
 
 variable "vpc_flow_log_role_arn" {
-    type = string
-    description = "VPC Flow Log role ARN"
+  type        = string
+  description = "VPC Flow Log role ARN"
 }
 
 variable "ecs_task_execution_role_arn" {
-    type = string
-    description = "ECS task execution role ARN"
+  type        = string
+  description = "ECS task execution role ARN"
 }
 
 variable "github_token" {
-    type = string
-    description = "GitHub token"
+  type        = string
+  description = "GitHub token"
 }
