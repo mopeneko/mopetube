ab64a45a13f04ad8f054b75da35f1224e7332047
mopeneko <mopeneko@lem0n.cc>
2024-04-04 18:33:38 +0900

    Auto scale ECS

--- tf/main.tf
+++ tf/main.tf
@@ -354,3 +354,28 @@ resource "aws_ecs_service" "mopetube_service" {
     container_port   = "3000"
   }
 }
+
+resource "aws_appautoscaling_target" "ecs_target" {
+  service_namespace  = "ecs"
+  resource_id        = "service/${aws_ecs_cluster.mopetube_cluster.name}/${aws_ecs_service.mopetube_service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  min_capacity       = 1
+  max_capacity       = 10
+}
+
+resource "aws_appautoscaling_policy" "ecs_policy" {
+  name               = "mopetube-service-autoscaling"
+  service_namespace  = "ecs"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = "ecs:service:DesiredCount"
+  policy_type        = "TargetTrackingScaling"
+
+  target_tracking_scaling_policy_configuration {
+    target_value       = 75.0
+    scale_in_cooldown  = 300
+    scale_out_cooldown = 300
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+  }
+}
