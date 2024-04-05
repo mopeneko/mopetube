7e62ba5863ece9388e6aca7b53ae1c4c80a89e4e
mopeneko <mopeneko@lem0n.cc>
2024-04-06 03:21:36 +0900

    Fix favicon not found

--- app/app/root.tsx
+++ app/app/root.tsx
@@ -1,8 +1,14 @@
-import { Links, Meta, Scripts, ScrollRestoration } from "@remix-run/react";
+import {
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+} from "@remix-run/react";
 
 export function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang="ja">
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -16,4 +22,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
 			</body>
 		</html>
 	);
+}
+
+export default function App() {
+	return <Outlet />;
 }
