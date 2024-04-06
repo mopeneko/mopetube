f546940fb38487c77f251406a8151271df381443
mopeneko <mopeneko@lem0n.cc>
2024-04-06 16:36:47 +0900

    Connect to database

---
+++ app/api/db/connection.ts
@@ -0,0 +1,18 @@
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
+import fs from "node:fs";
+
+const connection = await mysql.createConnection({
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT ? Number.parseInt(process.env.DB_PORT) : undefined,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
+	ssl: {
+		ca: process.env.CA_PATH ? fs.readFileSync(process.env.CA_PATH) : undefined,
+	},
+});
+
+const db = drizzle(connection);
+
+export default db;
