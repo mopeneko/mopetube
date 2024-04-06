76d0c4bdd7590eb3dbdca529bf57eaa5b4e9e3c5
mopeneko <mopeneko@lem0n.cc>
2024-04-06 16:42:36 +0900

    Parse port number with specifing radix

--- app/api/db/connection.ts
+++ app/api/db/connection.ts
@@ -4,7 +4,9 @@ import mysql from "mysql2/promise";
 
 const connection = await mysql.createConnection({
 	host: process.env.DB_HOST,
-	port: process.env.DB_PORT ? Number.parseInt(process.env.DB_PORT) : undefined,
+	port: process.env.DB_PORT
+		? Number.parseInt(process.env.DB_PORT, 10)
+		: undefined,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASS,
 	database: process.env.DB_NAME,
