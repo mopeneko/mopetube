a2992988a3ad85cd0b388ae5a7015ab1e7b0d397
mopeneko <mopeneko@lem0n.cc>
2024-04-07 20:07:48 +0900

    Refactor codes

--- app/api/users/index.ts
+++ app/api/users/index.ts
@@ -1,11 +1,7 @@
 import { zValidator } from "@hono/zod-validator";
-import { db } from "api/db/connection.js";
-import { users } from "api/db/schema.js";
 import { logger } from "api/logger.js";
-import argon2 from "argon2";
-import { count } from "drizzle-orm";
+import { userService } from "api/services/index.js";
 import { Hono } from "hono";
-import { ulid } from "ulid";
 import { z } from "zod";
 
 const app = new Hono();
@@ -25,36 +21,19 @@ app.post(
 		},
 	),
 	async (c) => {
-		const cnt = await db.select({ value: count() }).from(users);
+		try {
+			if (!(await userService.isFirstUser())) {
+				return c.json({ message: "first user has already been created" }, 409);
+			}
 
-		if (cnt.length !== 1) {
+			const { username, password } = c.req.valid("json");
+
+			const user = await userService.createUser(username, password);
+			return c.json({ id: user.id, username: user.username });
+		} catch (error) {
+			logger.error(error);
 			return c.json({ message: "Internal Server Error" }, 500);
 		}
-
-		if (cnt[0].value !== 0) {
-			return c.json({ message: "first user has already been created" });
-		}
-
-		const { username, password } = c.req.valid("json");
-
-		const pepper = process.env.PEPPER;
-		if (!pepper) {
-			logger.error("PEPPER is not set");
-			return c.json({ message: "Internal Server Error" }, 500);
-		}
-
-		const hash = await argon2.hash(password, { secret: Buffer.from(pepper) });
-
-		const id = ulid();
-
-		await db.insert(users).values({
-			id,
-			username,
-			hash,
-		});
-
-		logger.info(`User ${username} created`);
-		return c.json({ id, username });
 	},
 );
 
